81c9f87de030aa767f8d16e7771f7551dace602f
Collin Fingar <fingac@rpi.edu>
2015-11-23 16:42:37 -0500

    Started pathfinding scene

---
+++ AirportGame/Assets/Scripts/TSAAgentScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TSAAgentScript : MonoBehaviour {
+    public float speed = 1f;
+
+    public GameObject player;
+    public float noticeDistance = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(player.transform.position, transform.position) < noticeDistance) {
+
+        }
+	}
+}
