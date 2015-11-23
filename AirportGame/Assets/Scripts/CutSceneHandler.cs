a50fc9d66a6d7aea8975af08b70be80bff3f0cce
Collin Fingar <fingac@rpi.edu>
2015-11-22 19:26:54 -0500

    Added "Main Menu" and "Cutscenes" scenes with generic placeholder textures and cutscenes. They are linked so Main menu goes to cutscenes which goes to the first level scene.

---
+++ AirportGame/Assets/Scripts/CutSceneHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutSceneHandler : MonoBehaviour {
+
+    private ArrayList scenes;
+    private int currentScene;
+
+	// Use this for initialization
+	void Start () {
+        scenes = new ArrayList();
+        foreach (Transform child in transform) {
+            scenes.Add(child.gameObject);
+        }
+        currentScene = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+            GameObject scene = (GameObject)scenes[currentScene];
+            scene.SetActive(false);
+            currentScene += 1;
+            if (currentScene == scenes.Count) {
+                Application.LoadLevel(2);
+            }
+        }
+	}
+}
---
+++ AirportGame/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 200, Screen.height - 170, 100, 60), "Start"))
+            Application.LoadLevel(1);
+        if (GUI.Button(new Rect(Screen.width - 200, Screen.height - 110, 100, 60), "Instructions"))
+            Debug.Log("instructions");
+    }
+}
