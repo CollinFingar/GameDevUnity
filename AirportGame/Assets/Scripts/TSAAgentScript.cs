95ba9ecfa28f6836fecf3df3874a76b60f3c0d1d
Collin Fingar <fingac@rpi.edu>
2015-11-23 17:08:34 -0500

    Agents now cone check for you and go after you if within a certain distance and angle.

--- AirportGame/Assets/Scripts/TSAAgentScript.cs
+++ AirportGame/Assets/Scripts/TSAAgentScript.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class TSAAgentScript : MonoBehaviour {
-    public float speed = 1f;
+    public float speed = 10f;
+    public float turnSpeed = 20f;
 
     public GameObject player;
     public float noticeDistance = 10f;
+
+    public Vector3 velocity = new Vector3();
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +17,12 @@ public class TSAAgentScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Vector3.Distance(player.transform.position, transform.position) < noticeDistance) {
-
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        Vector3 targetDirection = player.transform.position - transform.position;
+        float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+        if (distanceToPlayer < noticeDistance && distanceToPlayer > 2 && angleToPlayer < 45) {
+            transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, targetDirection, turnSpeed * Time.deltaTime, 0.0f));
+            transform.position += transform.forward * speed * Time.deltaTime;
         }
 	}
 }
