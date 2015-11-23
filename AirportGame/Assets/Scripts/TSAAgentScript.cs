4bb1ff7a7ae132b4da2c11abb1449344d23add0e
Collin Fingar <fingac@rpi.edu>
2015-11-23 17:43:22 -0500

    Agent moves back to starting point if you move far enough away during chase.

--- AirportGame/Assets/Scripts/TSAAgentScript.cs
+++ AirportGame/Assets/Scripts/TSAAgentScript.cs
@@ -8,11 +8,17 @@ public class TSAAgentScript : MonoBehaviour {
     public GameObject player;
     public float noticeDistance = 10f;
 
-    public Vector3 velocity = new Vector3();
+    public bool followingPlayer = false;
+    public bool movingBack = false;
+
+    private Vector3 watchSpot = new Vector3();
+    private Vector3 watchRotation = new Vector3();
+    private Vector3 velocity = new Vector3();
 
 	// Use this for initialization
 	void Start () {
-	
+        watchSpot = transform.position;
+        watchRotation = transform.localEulerAngles;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +29,28 @@ public class TSAAgentScript : MonoBehaviour {
         if (distanceToPlayer < noticeDistance && distanceToPlayer > 2 && angleToPlayer < 45) {
             transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, targetDirection, turnSpeed * Time.deltaTime, 0.0f));
             transform.position += transform.forward * speed * Time.deltaTime;
+            followingPlayer = true;
+        } else {
+            if (followingPlayer) {
+                movingBack = true;
+                followingPlayer = false;
+            }
+            if (movingBack)
+            {
+                targetDirection = watchSpot - transform.position;
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, targetDirection, turnSpeed * Time.deltaTime, 0.0f));
+                transform.position += transform.forward * speed * Time.deltaTime;
+                if (Vector3.Distance(watchSpot, transform.position) < 2)
+                {
+                    movingBack = false;
+                }
+            }
+            else {
+                if (Mathf.Abs(transform.localEulerAngles.y - watchRotation.y) < 5) {
+                    transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + 5, transform.localEulerAngles.z);
+                }
+            }
+            
         }
 	}
 }
