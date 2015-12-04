25e2b9f1c4e26b49ab43f0152726ad719d8cbf3f
Collin Fingar <fingac@rpi.edu>
2015-12-04 18:58:10 -0500

    There are now basic pop-ups on the screen for media

---
+++ AirportGame/Assets/Scripts/PopupController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupController : MonoBehaviour {
+
+    public Texture aTexture;
+
+    public bool showingUp = false;
+    public bool closingDown = false;
+
+    private bool on = false;
+    private byte alpha = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            makeVisible();
+        }
+
+        if (showingUp)
+        {
+            if (alpha <  200)
+            {
+                alpha += 20;
+            }
+            else
+            {
+                showingUp = false;
+            }
+        }
+        if (closingDown)
+        {
+            if (alpha > 0)
+            {
+                alpha -= 20;
+            }
+            else
+            {
+                closingDown = false;
+            }
+        }
+
+
+    }
+
+    void OnGUI() {
+        if (on || (closingDown)) {
+            GUI.color = new Color32(255, 255, 255, alpha);
+            GUI.DrawTexture(new Rect(Screen.width - 400, 0, 400, 200), aTexture, ScaleMode.StretchToFill, true, 10.0F);
+        }
+        
+    }
+
+
+    void makeVisible()
+    {
+        if (!on)
+        {
+            on = true;
+            showingUp = true;
+            closingDown = false;
+        }
+        else
+        {
+            on = false;
+            showingUp = false;
+            closingDown = true;
+        }
+    }
+
+
+}
