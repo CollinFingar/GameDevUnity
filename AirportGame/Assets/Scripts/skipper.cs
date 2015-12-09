66089e00b4862d2218da81ec2e661f7cce36dcc6
Collin Fingar <fingac@rpi.edu>
2015-12-09 13:29:22 -0500

    You can now skip between levels with 7 8 and 9

---
+++ AirportGame/Assets/Scripts/skipper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skipper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            Application.LoadLevel(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            Application.LoadLevel(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9)) {
+            Application.LoadLevel(4);
+        }
+	}
+}
