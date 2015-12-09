419f8e11df928061b2c0e19c9f5440e00baeb773
Collin Fingar <fingac@rpi.edu>
2015-12-09 04:21:23 -0500

    Fixed lighting problem

---
+++ AirportGame/Assets/Scripts/LevelProgresser.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelProgresser : MonoBehaviour {
+
+    public int nextLevelIndex = 0;
+
+    public float distance = 4f;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < distance) {
+            Application.LoadLevel(nextLevelIndex);
+        }
+	}
+}
